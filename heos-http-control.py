7f66f09e871687d9ed5a7fbd20f6fc6ee923b901
maciej-t <47009658+maciej-t@users.noreply.github.com>
2024-11-07 15:25:14 +0100

    Update heos-http-control.py
    added initial AVR control

--- heos-http-control.py
+++ heos-http-control.py
@@ -4,14 +4,15 @@ import telnetlib
 app = Flask(__name__)
 
 DENON_IP = '192.168.200.206' #Set your Denon Heos player IP if you do not want to use 'ip' in every query
-DENON_PORT = 1255 #Default port for Denon Heos, change if needed
-DENON_CMD = 'players/get_players' #Default command that will be used if no command specified
+DENON_PORT = 1255 #Default port for Denon Heos. Default port for Heos - 1255. Default port for AVR - 23.
+DENON_CMD = 'players/get_players' #Example HEOS commands: 'players/get_players'. Example  AVR command: 'PW?'
+DENON_PROT = 'HEOS' #Default Heos (HEOS|AVR)
 
 def execute_denon_command(command, ip, port):
     try:
         with telnetlib.Telnet(ip, port) as tn:
             tn.write(command.encode('ascii') + b'\r\n')
-            response = tn.read_until(b'\r\n', timeout=2).decode('utf-8')
+            response = tn.read_until(b'\r\n', timeout=1).decode('utf-8')
         return response.strip()
     except Exception as e:
         return f"Error: {str(e)}"
@@ -20,12 +21,14 @@ def execute_denon_command(command, ip, port):
 @app.route('/', methods=['GET'])
 def execute():
     denon_cmd = request.args.get('command', DENON_CMD)
-    denon_cmd = 'heos://'+denon_cmd
     denon_ip = request.args.get('ip', DENON_IP)
     denon_port = request.args.get('port', DENON_PORT)
-
+    denon_proto = request.args.get('proto', DENON_PROTO)
+    if denon_prot == 'HEOS':
+        denon_cmd = 'heos://'+denon_cmd
     result = execute_denon_command(denon_cmd, denon_ip, denon_port)
-    return result+"\r\n";
+    resultnl = result.replace("\r", "\r\n")
+    return resultnl+"\r\n";
 
 
 if __name__ == '__main__':
